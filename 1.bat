00f08d4f049e351ebbdd378345b8fc8be7bb204c
Eric Zhang <ericxiang456@gmail.com>
2021-09-23 22:47:20 +0800

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,6 +1,6 @@
 @echo off
 rem inspired by enderman on youtube
-echo Partioning the disk and mounting the filesystem
+echo Partioning the disk and mounting the file system
 (echo select disk 0
 echo clean
 echo convert mbr
@@ -10,7 +10,7 @@ echo assign letter c
 echo exit
 )  | diskpart
 rem whoa i didn't know windows had piping until i found this on stack overflow
-echo Installing base system to the mounted filesystem
+echo Installing base system to the mounted file system
 D:
 cd sources
 dism /apply-image /imagefile:install.wim /index:1 /applydir:C:\
@@ -21,8 +21,9 @@ echo Performing registry actions
 reg load HKLM\SOFT C:\Windows\system32\config\SOFTWARE
 reg load HKLM\SYS C:\Windows\system32\config\system
 reg add HKLM\SOFT\Microsoft\Windows\CurrentVersion\Policies\System /v VerboseStatus /t REG_DWORD /d 1
+rem change E with the drive letter that you have this script and other stuff in it
 reg import E:\reg.reg
-echo Copying second part of the script to the filesystem
+echo Copying second part of the script to the file system
 copy E:\2.bat C:\
 copy E:\reg2.reg C:\
 rem pause
